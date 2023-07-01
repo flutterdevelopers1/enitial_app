84fd6803c7226f2d5290d2c0ed346156da7346a1
krishpranav <krisna.pranav@gmail.com>
2023-07-01 10:51:43 +0530

    db: address_db

--- backend/src/db/address_db.rs
+++ backend/src/db/address_db.rs
@@ -3,3 +3,15 @@ use futures::TryStreamExt;
 use mongodb::{Collection, error::Error};
 use crate::model::address::Address;
 use super::get_db_handle;
+
+async fn get_handle() -> Collection<Address> {
+    return get_db_handle().await.collection::<Address>("adresses");
+}
+
+// get user address
+pub async fn get_addresses_from_user(user_id: &ObjectId) -> Result<Vec<Address>, Error> {
+    let handle = get_handle().await;
+    let results = handle.find(doc! { "userId": user_id }, None).await?;
+    let vec = results.try_collect::<Vec<Address>>().await?;
+    return Ok(vec);
+}
