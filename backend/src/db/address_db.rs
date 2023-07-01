23dc9c7e16f60d947ab03e21177bef694aa11882
krishpranav <krisna.pranav@gmail.com>
2023-07-01 10:47:41 +0530

    update log msg

--- backend/src/db/address_db.rs
+++ backend/src/db/address_db.rs
@@ -2,3 +2,4 @@ use bson::{oid::ObjectId, doc};
 use futures::TryStreamExt;
 use mongodb::{Collection, error::Error};
 use crate::model::address::Address;
+use super::get_db_handle;
--- backend/src/db/mod.rs
+++ backend/src/db/mod.rs
@@ -12,20 +12,20 @@ lazy_static! {
 async fn get_client_handle() -> Client {
   let mongo_url = env::var("MONGO_URI").expect("MONGO_URI is undefined!");
   let mongo_db_name = env::var("MONGO_DB_NAME").expect("MONGO_DB_NAME is undefined!");
-  let mut options = ClientOptions::parse(&mongo_url).await.expect("Unable to parse MONGO_URI!");
+  let mut options = ClientOptions::parse(&mongo_url).await.expect("Log: Unable to parse MONGO_URI!");
   options.max_pool_size = Some(100);
   options.min_pool_size = Some(10);
   options.max_idle_time = Some(std::time::Duration::from_secs(300));
   options.default_database = Some(mongo_db_name);
-  let client = Client::with_options(options).expect("Unable to create client!");
+  let client = Client::with_options(options).expect("Log: Unable to create client!");
   return client;
 }
 
 pub async fn get_db_handle() -> Database {
   let test_mode = env::var("TEST_MODE");
   return match test_mode {
-    Ok(_) => get_client_handle().await.default_database().expect("Unable to get default database!"),
-    Err(_) => CLIENT.get().await.default_database().expect("Unable to get default database!")
+    Ok(_) => get_client_handle().await.default_database().expect("Log: Unable to get default database!"),
+    Err(_) => CLIENT.get().await.default_database().expect("Log: Unable to get default database!")
   };
 }
 
